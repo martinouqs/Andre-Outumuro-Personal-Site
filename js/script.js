11d7c3f1188f33332a4da708674ebdae92dabeb5
martinouqs <58011249+martinouqs@users.noreply.github.com>
2023-01-03 17:58:53 +0100

    Main structure added. Added some changes to the gallery.

--- js/script.js
+++ js/script.js
@@ -5,3 +5,13 @@
   
   document.querySelector('.animated-icon2').classList.toggle('open');
   });
+
+  //CAROUSEL ACTION
+  var myCarousel = document.querySelector('#carouselExampleControls')
+  var myModalEl = document.getElementById('exampleModal')
+  
+  myModalEl.addEventListener('show.bs.modal', function (event) {
+      const trigger = event.relatedTarget
+      var bsCarousel = bootstrap.Carousel.getInstance(myCarousel)
+      bsCarousel.to(trigger.dataset.bsSlideTo)
+  })
