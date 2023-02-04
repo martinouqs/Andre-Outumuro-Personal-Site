8507b1850f254851b214cb71a2b20a8876be8494
martinouqs <58011249+martinouqs@users.noreply.github.com>
2023-02-05 00:38:53 +0100

    added validation of contact form. Added some basic styles too

---
+++ PHP/send.php
@@ -0,0 +1,34 @@
+<?php 
+
+$correo = $_POST['correo'];
+$nombre = $_POST['nombre'];
+$mensaje = $_POST['mensaje'];
+
+$destinatario = "andreoutumuro@gmail.com";
+$cuerpo = '
+    <html> 
+        <head> 
+            <title>Contactaron contigo a través de andreotumuro.com</title> 
+        </head>
+        <body> 
+            <h1>SolicitudE de contacto.  </h1>
+            <p> 
+              A persoa:  '.$nombre . ' <br>
+              Deixouche a seguinte mensaxe: '.$mensaje.' 
+            </p> 
+        </body>
+    </html>
+';
+//para el envío en formato HTML 
+$headers = "MIME-Version: 1.0\r\n"; 
+$headers .= "Content-type: text/html; charset=UTF8\r\n"; 
+
+//dirección del remitente
+
+$headers .= "FROM: $nombre <$correo>\r\n";
+mail($destinatario,$asunto,$cuerpo,$headers);
+
+echo "Correo enviado"; 
+
+header('Location:contacto.html');
+?> 
--- js/script.js
+++ js/script.js
@@ -15,3 +15,16 @@
       var bsCarousel = bootstrap.Carousel.getInstance(myCarousel)
       bsCarousel.to(trigger.dataset.bsSlideTo)
   })
+
+  // MULTILANGUAGE
+
+ // CONTACT FORM
+ const $form = document.querySelector('#form')
+  
+ $form.addEventListener('submit', handleSubmit)
+
+ function handleSubmit(event){
+    event.preventDefault()
+    const form = new FormData(this)
+    
+ }
